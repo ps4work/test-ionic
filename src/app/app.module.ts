1ec498e65e7f3989c7d4c8f7745ea0a4f3df9960
pechenkin <pechenkin@protei.ru>
2017-09-08 18:34:17 +0300

    Add firebase authentication.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AuthProvider } from '../providers/auth/auth';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmhmDBp8bq_cUIqfUhNtVwiUN7QBCU530",
+  authDomain: "ps-test-ionic.firebaseapp.com",
+  databaseURL: "https://ps-test-ionic.firebaseio.com",
+  projectId: "ps-test-ionic",
+  storageBucket: "ps-test-ionic.appspot.com",
+  messagingSenderId: "884273426528"
+};
 
 @NgModule({
   declarations: [
@@ -14,6 +28,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +39,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AngularFireAuth,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/auth/auth.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class AuthProvider {
+
+  constructor(public afAuth: AngularFireAuth) { }
+
+  loginWithGoogle() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+}
